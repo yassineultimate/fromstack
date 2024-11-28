ba16c6c64bb3d2738a902f19e2ea088a9009af4e
yassine harbeg <yharbeg@sqli.Com>
2024-11-28 09:14:19 +0100

    daymap

--- src/types/salon.ts
+++ src/types/salon.ts
@@ -41,3 +41,22 @@ export interface Salon {
   phone: string;
   status: 'active' | 'inactive';
 }
+export const dayMap = {
+  0:'Dimanche',
+  1:'Lundi' ,
+  2:'Mardi'  ,
+  3:'Mercredi' ,
+  4: 'Jeudi',
+  5:'Vendredi' ,
+  
+    6: 'Samedi'
+  } as const;
+export const dayMap2 = {
+  'Dimanche':0,
+  'Lundi' :1,
+   'Mardi' :2 ,
+   'Mercredi' :3,
+  'Jeudi':4,
+  'Vendredi' :5,
+  'Samedi':6
+} as const;

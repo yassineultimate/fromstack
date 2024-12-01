07029585d6e68cca957b1241019afc0ec283fa56
yassine harbeg <yharbeg@sqli.Com>
2024-12-01 17:28:42 +0100

    comiy

--- Service/ConfigService.ts
+++ Service/ConfigService.ts
@@ -1,8 +1,8 @@
 // src/services/ConfigService.ts
 
 class ConfigService {
-    private static BASE_URL: string = 'http://192.168.1.46:3307';
-  
+    //private static BASE_URL: string = 'http://192.168.1.46:3307';
+    private static BASE_URL: string = 'https://zonal-serenity-production.up.railway.app';
     // Optional: Add methods to update or retrieve configuration properties dynamically
     public static getBaseUrl(): string {
       return this.BASE_URL;
